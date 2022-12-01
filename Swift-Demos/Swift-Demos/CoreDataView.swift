c91037e0b560bdf57a3413542174422e853d840e
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2022-12-01 11:16:24 -0500

    Got core data to work

--- Swift-Demos/Swift-Demos/CoreDataView.swift
+++ Swift-Demos/Swift-Demos/CoreDataView.swift
@@ -13,9 +13,10 @@ struct CoreDataView: View {
     //State is declared to tell swift that these vars can change in the view
     @State var firstName: String = ""
     @State var lastName: String = ""
-    @State var age: Int = 0
     @State var stateLivedIn: String = ""
     @State var dateOfBirth: Date = Date.now
+    
+    @State var popUp: Bool = false
     
     
     @Environment(\.managedObjectContext) private var viewContext
@@ -26,17 +27,78 @@ struct CoreDataView: View {
     @FetchRequest(entity: People.entity(), sortDescriptors: []) private var people: FetchedResults<People>
     
     var body: some View {
-        List{
-            ForEach(people){ person in
-                VStack{
-                    Text("Name: \(person.firstName ?? "no name") \(person.lastName ?? "no last name")")
-                    Text("Age: \(person.age ?? 0)")
-                    Text("From: \(person.stateLiveIn ?? "no state given")")
+        Group{
+            if people.isEmpty{
+                Text("No People in Core Data")
+            }else{
+                List{
+                    ForEach(people){ person in
+                        VStack{
+                            Text("Name: \(person.firstName ?? "no name") \(person.lastName ?? "no last name")")
+                            Text("From: \(person.stateLiveIn ?? "no state given")")
+                        }
+                    }
                 }
             }
+        }.toolbar{
+            ToolbarItem{
+                Button(action: {
+                    popUp.toggle()
+                }, label: {
+                    Text("Add Person")
+                })
+            }
+        }.sheet(isPresented: $popUp, content: {
+            AddPersonView(firstName: $firstName, lastName: $lastName, stateLivedIn: $stateLivedIn, dateOfBirth: $dateOfBirth)
+        })
+    }
+}
+
+struct AddPersonView:View{
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: People.entity(), sortDescriptors: []) private var people: FetchedResults<People>
+    
+    
+    @Binding var firstName:String
+    @Binding var lastName:String
+    @Binding var stateLivedIn: String
+    @Binding var dateOfBirth: Date
+    
+    var body: some View{
+        Form{
+            Section("Name", content: {
+                TextField("first name", text: $firstName)
+                TextField("last name", text: $lastName)
+            })
+            
+            Section("About", content: {
+                DatePicker("Date of Birth", selection: $dateOfBirth, displayedComponents: .date)
+                TextField("State That you Live In", text: $stateLivedIn)
+            })
+            
+            Button("Submit", action: {
+                withAnimation{
+                    let person = People(context: viewContext)
+                    person.firstName = firstName
+                    person.lastName = lastName
+                    person.dateOfBirth = dateOfBirth
+                    person.stateLiveIn = stateLivedIn
+                    
+                    do{
+                        try viewContext.save()
+                    }catch {
+                        let error = error as NSError
+                        fatalError("Error saving to CoreDate: \(error)")
+                    }
+                }
+            })
         }
     }
 }
+
+
 
 struct CoreDataView_Previews: PreviewProvider {
     static var previews: some View {
