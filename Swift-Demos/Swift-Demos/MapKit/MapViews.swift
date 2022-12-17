89a7501d48ac7d5a35a627f2e6e36fbf5ae80c8e
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2022-12-17 12:17:18 -0500

    push

--- Swift-Demos/Swift-Demos/MapKit/MapViews.swift
+++ Swift-Demos/Swift-Demos/MapKit/MapViews.swift
@@ -12,21 +12,45 @@ import MapKit
 struct MapViews: View {
     
     @State private var mapRe = MKCoordinateRegion(.world)
+    @State var searchLocation: String = ""
+    let searchRequest = MKLocalSearch.Request()
     
     var body: some View {
         
             
         TabView {
-            Map(coordinateRegion: $mapRe).tabItem({
+            ZStack {
+                
+                Map(coordinateRegion: $mapRe).ignoresSafeArea()
+                VStack {
+                    
+                    TextField("Type a location", text: $searchLocation).frame(width: 350, height: 50).padding().font(.title).background(.gray).cornerRadius(10).onSubmit {
+                        Task{
+                            try await findLocation(location:searchLocation)
+                        }
+                    }
+                    Spacer()
+                }
+            }.tabItem({
                 Label("Map", systemImage: "globe").bold().font(.title)
             })
-            
-            
         }
-                
+    }
+    
+    func findLocation(location: String) async throws{
+        searchRequest.naturalLanguageQuery = location
+        let search = MKLocalSearch(request: searchRequest)
+        
+        try await search.start { resp, err in
+            guard let response = resp else {
+                print("Error")
+                return
+            }
             
-        
-        
+            for item in response.mapItems{
+                print(item.placemark)
+            }
+        }
     }
 }
 
